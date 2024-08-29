0eb9e3f754db64a5fc3a7205a743974cfc203da0
LVQuang <ql7769663@gmail.com>
2024-08-29 15:02:14 +0700

    Init Project

---
+++ src/test/java/dev/lvpq/CS502052/Cs502052ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lvpq.CS502052;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs502052ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
