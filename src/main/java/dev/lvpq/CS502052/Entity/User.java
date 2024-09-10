1c5aac017410678bf548f3d51a398eec27b984ed
LVQuang <ql7769663@gmail.com>
2024-09-10 18:31:02 +0700

    SE02: Config Mapstruct-Lombok for Project

---
+++ src/main/java/dev/lvpq/CS502052/Entity/User.java
@@ -0,0 +1,22 @@
+package dev.lvpq.CS502052.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+    @Column(nullable = false, unique = true)
+    String name;
+    String password;
+    String email;
+    String phone;
+}
